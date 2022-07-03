24a13ffc35894f0ac26bc7e8d2af636cf7ff6831
Coty McKinney <cmckin8741@gmail.com>
2022-07-02 23:55:54 -0400

    Began user search implementation

--- src/App.js
+++ src/App.js
@@ -6,14 +6,26 @@ import Marquee from './components/marquee/Marquee';
 
 function App() {
   const [state, setState] = useState([]);
+  // let USER_QUERY;
   
   const getMovieData = async () => {
     try {
       const API_KEY = '31c8c9bf37b71837f31ec5a6e33bfac5';
       const BASE_URL = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US`;
-      const movies = await fetch(BASE_URL).then(res => res.json());
+      // let SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${USER_QUERY}&page=1&include_adult=false`
+      let movies;
+      // let userSearch;
+
+      // ADD SEARCH LOGIC
+      // if(search button clicked) {
+      //  userSearch = await fetch(SEARCH_URL).then(res => res.json());
+
+      //  setState(userSearch.results);
+      // }else {
+      movies = await fetch(BASE_URL).then(res => res.json());
       
       setState(movies.results);
+      // };
       
     } catch (error) {
       console.log(error);
@@ -22,7 +34,7 @@ function App() {
   
   useEffect(() => {
     getMovieData();
-  }, []
+  }
   );
 
   return (
--- src/components/marquee/Marquee.js
+++ src/components/marquee/Marquee.js
@@ -1,17 +1,19 @@
 const Marquee = () => {
     return (
         <div className='marquee'>
-            <div className="lightsLeft">
-                <span className="littleDot"></span>
-                <span className="dot"></span>
-                <span className="bigDot"></span>
-            </div>
+            <span className="leftDot"></span>
+            <span className="leftLittleDot"></span>
+            <span className="leftBigDot"></span>
+            <span className="leftDot"></span>
+            <span className="leftLittleDot"></span>
+            <span className="leftBigDot"></span>
             <h1>Now Showing!</h1>
-            <div className="lightsRight">
-                <span className="dot"></span>
-                <span className="bigDot"></span>
-                <span className="littleDot"></span>
-            </div>
+            <span className="rightDot"></span>
+            <span className="rightBigDot"></span>
+            <span className="rightLittleDot"></span>
+            <span className="rightDot"></span>
+            <span className="rightBigDot"></span>
+            <span className="rightLittleDot"></span>
         </div>
     )
 };
